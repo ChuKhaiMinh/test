1a9e812b859fc99b6c2addb33be310e0d99d25e7
ChuKhaiMinh <chukhaiminh1389@gmail.com>
2025-06-14 19:51:17 +0700

    Update news.js

--- news.js
+++ news.js
@@ -3,7 +3,7 @@ const newsContent2 = document.querySelector("#news_content2");
 const newsContent3 = document.querySelector("#news_content3");
 const newsContent4 = document.querySelector("#news_content4");
 const getData = async () => {
-  const response = await fetch("../src/news.json");
+  const response = await fetch("news.json");
   const data = await response.json();
   if (data) {
     newsContent1.textContent = data[0].content;
