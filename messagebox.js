0068bfbe1fd99cfb7603edc6409340683e1c7286
ChuKhaiMinh <chukhaiminh1389@gmail.com>
2025-06-14 19:50:59 +0700

    Update messagebox.js

--- messagebox.js
+++ messagebox.js
@@ -23,14 +23,14 @@ const avatarUser = document.querySelector("#avatar");
 const disnameUser = document.querySelector("#disname");
 onAuthStateChanged(auth, async (user) => {
   if (!user) {
-    return window.location.replace("../html/signin.html");
+    return window.location.replace("signin.html");
   }
   const uid = user.uid;
   try {
     const fieldAll = (await getDoc(doc(getFirestore(), "users", uid))).data();
     disnameUser.textContent = fieldAll.disname;
     if (!fieldAll.avatarUrl) {
-      avatarUser.src = "../src/img/avatar.png";
+      avatarUser.src = "avatar.png";
     } else {
       avatarUser.src = fieldAll.avatarUrl;
     }
